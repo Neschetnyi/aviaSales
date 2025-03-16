dcfecbbc317a9ef226c20214969a9b5b8e8e462a
Neschetnyi <a.neschetnyi@gmail.com>
2025-03-16 23:10:29 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/aviaSales/",
+  build: {
+    outDir: "dist", // Убедись, что билд идёт в dist
+    assetsInlineLimit: 0,
+  },
+  server: {
+    open: true,
+  },
 });
